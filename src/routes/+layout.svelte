c4206e65dca300a83494c2d7e569e6be4140462a
Angelo Verlain <hey@vixalien.com>
2025-11-17 12:36:35 +0200

    chore: only preload important fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,6 @@
 	import { navigationHistory } from '$lib/stores/navigation';
 
 	import AtkinsonRegular from '$lib/assets/atkinson-regular.woff?url';
-	import AtkinsonBold from '$lib/assets/atkinson-bold.woff?url';
 	import GinestraBlack from '$lib/assets/ginestra-black.otf?url';
 
 	onNavigate((navigation) => {
@@ -34,8 +33,13 @@
 	<link rel="icon" href={favicon} />
 	<title>vixalien's memories</title>
 	<link rel="preload" href={AtkinsonRegular} as="font" type="font/woff" crossorigin="anonymous" />
-	<link rel="preload" href={AtkinsonBold} as="font" type="font/woff" crossorigin="anonymous" />
-	<link rel="preload" href={GinestraBlack} as="font" type="font/opentype" crossorigin="anonymous" />
+	<link
+		rel="preload"
+		href={GinestraBlack}
+		as="font"
+		type="application/x-font-opentype"
+		crossorigin="anonymous"
+	/>
 </svelte:head>
 
 {@render children()}
